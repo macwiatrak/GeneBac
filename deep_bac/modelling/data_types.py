f3e07ffb6864d89653a5bc0e67dd25d10cd94776
macwiatrak <macwiatrak@gmail.com>
2023-01-05 20:56:15 +0100

    feat: change default parameters

--- deep_bac/modelling/data_types.py
+++ deep_bac/modelling/data_types.py
@@ -11,11 +11,11 @@ class DeepBacConfig:
     graph_model_type: Literal["transformer"] = "transformer"
     regression: bool = False
     lr: float = 0.001
-    batch_size: int = 16
+    batch_size: int = 8
     n_gene_bottleneck_layer: int = 256
     n_init_filters: int = 256
-    n_transformer_heads: int = 8
-    n_graph_layers: int = 4
+    n_transformer_heads: int = 4
+    n_graph_layers: int = 2
     n_output: int = 14  # nr of drugs in the cryptic dataset
     max_epochs: int = 100
     train_set_len: Optional[int] = None
