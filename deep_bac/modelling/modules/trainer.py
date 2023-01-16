2f2c5241e9e729ad105860052268a853c96b1784
macwiatrak <macwiatrak@gmail.com>
2023-01-16 22:13:36 +0000

    feat: comment out model checkpoint

--- deep_bac/modelling/modules/trainer.py
+++ deep_bac/modelling/modules/trainer.py
@@ -1,6 +1,6 @@
 import torch.cuda
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.callbacks import EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 
 from deep_bac.modelling.data_types import DeepBacConfig
@@ -29,13 +29,14 @@ def get_trainer(config: DeepBacConfig, output_dir: str) -> Trainer:
                 patience=config.early_stopping_patience,
                 mode="min",
             ),
-            ModelCheckpoint(
-                monitor="val_loss",
-                mode="min",
-                save_top_k=1,
-                save_last=True,
-            ),
+            # ModelCheckpoint(
+            #     monitor="val_loss",
+            #     mode="min",
+            #     save_top_k=1,
+            #     save_last=True,
+            # ),
         ],
         logger=TensorBoardLogger(output_dir),
         accumulate_grad_batches=config.accumulate_grad_batches,
+        log_every_n_steps=200,
     )
