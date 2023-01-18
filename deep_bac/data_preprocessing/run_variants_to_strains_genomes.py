c3b9a1ca75fc71e19a776bca5d1753e9cff37964
macwiatrak <macwiatrak@gmail.com>
2023-01-18 11:24:58 +0000

    feat: remove redundant comments

--- deep_bac/data_preprocessing/run_variants_to_strains_genomes.py
+++ deep_bac/data_preprocessing/run_variants_to_strains_genomes.py
@@ -5,8 +5,15 @@ import pandas as pd
 from tqdm import tqdm
 import logging
 
-from deep_bac.data_preprocessing.utils import read_ref_genome, read_genome_assembly, get_gene_data_dict
-from deep_bac.data_preprocessing.variants_to_strains_genomes import get_and_filter_variants_df, apply_variants_to_a_gene
+from deep_bac.data_preprocessing.utils import (
+    read_ref_genome,
+    read_genome_assembly,
+    get_gene_data_dict,
+)
+from deep_bac.data_preprocessing.variants_to_strains_genomes import (
+    get_and_filter_variants_df,
+    apply_variants_to_a_gene,
+)
 
 REF_GENOME_FILE_NAME = "H37Rv_NC_000962.3_ref_genome.fna"
 GENOME_ASSEMBLY_FILE_NAME = os.path.join("genome_assembly", "genomic.gff")
@@ -18,43 +25,53 @@ tqdm.pandas()
 
 
 def get_strain_w_phenotype_ids(file_path: str) -> Set[str]:
-    df = pd.read_csv(file_path, compression='gzip')
+    df = pd.read_csv(file_path, compression="gzip")
     # filter low quality phenotypes
-    df = df[~(df['PHENOTYPE_QUALITY'] == "LOW")]
-    return set(df['UNIQUEID'].tolist())
+    df = df[~(df["PHENOTYPE_QUALITY"] == "LOW")]
+    return set(df["UNIQUEID"].tolist())
 
 
 def agg_variants(variants_df: pd.DataFrame) -> pd.DataFrame:
-    agg_df = variants_df.sort_values(by=['NUCLEOTIDE_NUMBER']).groupby(["UNIQUEID", "GENE"]).agg(list)
+    agg_df = (
+        variants_df.sort_values(by=["NUCLEOTIDE_NUMBER"])
+        .groupby(["UNIQUEID", "GENE"])
+        .agg(list)
+    )
     # add gene column with gene names
     genes = [i[-1] for i in agg_df.index]
-    agg_df['gene'] = genes
+    agg_df["gene"] = genes
     return agg_df
 
 
 def postprocess_agg_variants(agg_variants_df: pd.DataFrame) -> pd.DataFrame:
     # remove redundant columns
-    agg_variants_df = agg_variants_df.drop(columns=['REF', 'ALT', 'NUCLEOTIDE_NUMBER', 'IS_INDEL'])
+    agg_variants_df = agg_variants_df.drop(
+        columns=["REF", "ALT", "NUCLEOTIDE_NUMBER", "IS_INDEL"]
+    )
     # clean up the columns
-    agg_variants_df['prom_gene_seq_w_variants'] = agg_variants_df['features'].progress_apply(
-        lambda x: x['prom_gene_seq_w_variants'])
-    agg_variants_df['ref_correct_ratio'] = agg_variants_df['features'].progress_apply(
-        lambda x: x['ref_correct_ratio'])
+    agg_variants_df["prom_gene_seq_w_variants"] = agg_variants_df[
+        "features"
+    ].progress_apply(lambda x: x["prom_gene_seq_w_variants"])
+    agg_variants_df["ref_correct_ratio"] = agg_variants_df[
+        "features"
+    ].progress_apply(lambda x: x["ref_correct_ratio"])
     # remove the features column as we don't need it anymore
-    agg_variants_df = agg_variants_df.drop(columns=['features'])
+    agg_variants_df = agg_variants_df.drop(columns=["features"])
     return agg_variants_df
 
 
 def run(
-        input_dir: str,
-        output_dir: str,
-        prom_seq_len: int,
+    input_dir: str,
+    output_dir: str,
+    prom_seq_len: int,
 ):
     # read ref genome
     ref_genome = read_ref_genome(os.path.join(input_dir, REF_GENOME_FILE_NAME))
 
     # read genome assembly
-    genome_assembly = read_genome_assembly(os.path.join(input_dir, GENOME_ASSEMBLY_FILE_NAME))
+    genome_assembly = read_genome_assembly(
+        os.path.join(input_dir, GENOME_ASSEMBLY_FILE_NAME)
+    )
 
     # get gene data dict with ref gene and promoter seqs for each gene
     # as well as info about the gene's strand
@@ -65,7 +82,9 @@ def run(
     )
 
     # get unique ids to use
-    strain_w_phenotype_ids = get_strain_w_phenotype_ids(os.path.join(input_dir, PHENOTYPE_FILE_NAME))
+    strain_w_phenotype_ids = get_strain_w_phenotype_ids(
+        os.path.join(input_dir, PHENOTYPE_FILE_NAME)
+    )
 
     logging.info("Reading variants")
     # get variants
@@ -80,31 +99,30 @@ def run(
     logging.info("Finished aggregating variants")
 
     # apply variants
-    agg_variants_df['features'] = agg_variants_df.progress_apply(
+    agg_variants_df["features"] = agg_variants_df.progress_apply(
         lambda row: apply_variants_to_a_gene(
-            ref_gene_seq=gene_data_dict[row['gene']]['gene_seq'],
-            ref_prom_seq=gene_data_dict[row['gene']]['promoter_seq'],
-            strand=gene_data_dict[row['gene']]['strand'],
-            row=row),
-        axis=1)
+            ref_gene_seq=gene_data_dict[row["gene"]]["gene_seq"],
+            ref_prom_seq=gene_data_dict[row["gene"]]["promoter_seq"],
+            strand=gene_data_dict[row["gene"]]["strand"],
+            row=row,
+        ),
+        axis=1,
+    )
     logging.info("Finished applying variants")
 
     agg_variants_df = postprocess_agg_variants(agg_variants_df)
 
     logging.info("Getting statistics about ref correct ratio")
     # logging some basic statistics about applied variants
-    mean_ref_correct_ratio = agg_variants_df['ref_correct_ratio'].mean()
-    std_ref_correct_ratio = agg_variants_df['ref_correct_ratio'].std()
-    logging.info(f"Mean ref correct ratio: {mean_ref_correct_ratio}"
-                 f"Std ref correct ratio: {std_ref_correct_ratio}")
+    mean_ref_correct_ratio = agg_variants_df["ref_correct_ratio"].mean()
+    std_ref_correct_ratio = agg_variants_df["ref_correct_ratio"].std()
+    logging.info(
+        f"Mean ref correct ratio: {mean_ref_correct_ratio}"
+        f"Std ref correct ratio: {std_ref_correct_ratio}"
+    )
 
     # save agg variants
     logging.info("Saving agg variants")
-    # agg_variants_df.to_parquet(
-    #     path=os.path.join(input_dir, output_dir),
-    #     engine='pyarrow',
-    #     partition_cols=['UNIQUEID'],
-    # )
     agg_variants_df.to_parquet(os.path.join(output_dir, "agg_variants.parquet"))
 
 
@@ -116,5 +134,5 @@ def main():
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
