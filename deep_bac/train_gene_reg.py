0d49a1f294c6e63712e6369749db3093ae326dea
macwiatrak <macwiatrak@gmail.com>
2023-02-07 10:39:39 +0000

    feat: add test_after_train option

--- deep_bac/train_gene_reg.py
+++ deep_bac/train_gene_reg.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from typing import Optional, Literal
@@ -28,6 +29,7 @@ def run(
     test: bool = False,
     ckpt_path: Optional[str] = None,
     use_drug_specific_genes: Literal["INH", "All"] = None,
+    test_after_test: bool = False,
 ):
     selected_genes = get_selected_genes(use_drug_specific_genes)
     logging.info(f"Selected genes: {selected_genes}")
@@ -58,6 +60,7 @@ def run(
         reverse_complement_prob=reverse_complement_prob,
         num_workers=num_workers if num_workers is not None else os.cpu_count(),
         selected_genes=selected_genes,
+        test=any([test, test_after_test]),
     )
     logging.info("Finished loading data")
 
@@ -78,6 +81,11 @@ def run(
         )
     else:
         trainer.fit(model, data.train_dataloader, data.val_dataloader)
+
+    if test_after_test:
+        results = trainer.test(
+            model, dataloaders=data.test_dataloader, ckpt_path=ckpt_path
+        )
     # in the future we could save the results
     return results
 
@@ -85,7 +93,7 @@ def run(
 def main(args):
     seed_everything(args.random_state)
     config = DeepBacConfig.from_dict(args.as_dict())
-    _ = run(
+    results = run(
         config=config,
         input_dir=args.input_dir,
         output_dir=args.output_dir,
@@ -99,7 +107,11 @@ def main(args):
         ckpt_path=args.ckpt_path,
         use_drug_idx=args.use_drug_idx,
         use_drug_specific_genes=args.use_drug_specific_genes,
+        test_after_test=args.test_after_test,
     )
+    if results is not None:
+        with open(os.path.join(args.output_dir, "results.json"), "w") as f:
+            json.dump(results, f)
 
 
 if __name__ == "__main__":
