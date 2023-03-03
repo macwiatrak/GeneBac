1548d27e9e888be0403d846d265499cc5f10d955
macwiatrak <macwiatrak@gmail.com>
2023-03-03 15:08:21 +0000

    feat: init latent gene and strain representations

---
+++ experiments/latent_representations/latent_gene_representations.py
@@ -0,0 +1,6 @@
+def run(ckpt_path: str, output_dir: str):
+    # TODO : Load the validation data
+    # TODO : Load the Gene Expression model
+    # TODO: Iterate over batches of validation data and save the output data into a DataFrame
+    # TODO: Save the DataFrame to a parquet file
+    pass
