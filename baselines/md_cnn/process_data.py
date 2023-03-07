cb24bdce03036ff8d26b0bf06f926b8ef17ce983
macwiatrak <macwiatrak@gmail.com>
2023-03-07 15:56:11 +0000

    fix: work on apply_variants_to_a_loci for MD-CNN data

--- baselines/md_cnn/process_data.py
+++ baselines/md_cnn/process_data.py
@@ -2,7 +2,7 @@ import logging
 import os
 from collections import defaultdict
 from functools import partial
-from typing import Dict, Tuple, List, Set, Union
+from typing import Dict, Tuple, List, Set, Union, Optional
 
 import pandas as pd
 
@@ -15,9 +15,11 @@ from deep_bac.data_preprocessing.run_variants_to_strains_genomes import (
 )
 from deep_bac.data_preprocessing.utils import (
     read_ref_genome,
+    get_complement,
 )
 from deep_bac.data_preprocessing.variants_to_strains_genomes import (
     VARIANTS_COLS_TO_USE,
+    retrieve_variant_idxs,
 )
 
 
@@ -44,6 +46,17 @@ def var_in_loci(loci_dict: Dict, genome_index: int) -> Union[str, None]:
     return None
 
 
+def get_loci_nucleotide_nr(row: Dict):
+    loci = row["loci"]
+    genome_index = row["GENOME_INDEX"]
+    strand = MD_CNN_GENOMIC_LOCI[loci][2]
+    if strand == "+":
+        # add 1 for compatibility with 1-indexing
+        return genome_index - MD_CNN_GENOMIC_LOCI[loci][0] + 1
+    # add 1 for compatibility with 1-indexing
+    return MD_CNN_GENOMIC_LOCI[loci][1] - genome_index + 1
+
+
 def get_and_filter_variants_to_loci_df(
     file_path: str,
     unique_ids_to_use: Set[str],
@@ -63,12 +76,16 @@ def get_and_filter_variants_to_loci_df(
     var_in_loci_fn = partial(var_in_loci, MD_CNN_GENOMIC_LOCI)
     df["loci"] = df["GENOME_INDEX"].progress_apply(var_in_loci_fn)
     df = df[~df["loci"].isna()]
+
+    df["loci_nucleotide_nr"] = df.progress_apply(
+        lambda row: get_loci_nucleotide_nr(row), axis=1
+    )
     return df
 
 
 def agg_variants(variants_df: pd.DataFrame) -> pd.DataFrame:
     agg_df = (
-        variants_df.sort_values(by=["GENOME_INDEX"])
+        variants_df.sort_values(by=["loci_nucleotide_nr"])
         .groupby(["UNIQUEID", "loci"])
         .agg(list)
     )
@@ -81,13 +98,45 @@ def agg_variants(variants_df: pd.DataFrame) -> pd.DataFrame:
 def apply_variants_to_a_loci(
     ref_seq: str,
     strand: str,
-    start: int,
-    end: int,
-    row: Dict,
+    row: Dict[str, Optional[List]],
 ) -> Dict:
     seq_w_variants = ""
+
+    len_ref_seq = len(ref_seq)
+    curr_index = 0
+    len_change = 0  # for sanity assertions
     ref_correct = 0
     n_nucleotide_change = 0
+
+    for ref, alt, nucleotide_nr, is_indel in zip(
+        row["REF"], row["ALT"], row["loci_nucleotide_nr"], row["IS_INDEL"]
+    ):
+        # get len of the ref to see where we stopped
+        len_variant = len(ref)
+        n_nucleotide_change += len_variant
+        # get this for sanity assertions later
+        len_change += len(alt) - len(ref)
+
+        # get complement and reverse if strand is negative and is indel
+        if strand == "-" and is_indel:
+            alt = get_complement(alt)[::-1]
+
+        vstart_idx, vend_idx = retrieve_variant_idxs(
+            nucleotide_nr=int(nucleotide_nr),
+            ref_len=len_variant,
+            strand=strand,
+            is_indel=is_indel,
+        )
+
+        vstart_idx = max(0, vstart_idx)
+        vend_idx = vend_idx if vend_idx >= 0 else len_ref_seq
+        seq_w_variants += ref_seq[curr_index:vstart_idx]
+        seq_w_variants += alt
+        curr_index = vend_idx
+        if ref == ref_seq[vstart_idx:vend_idx]:
+            ref_correct += 1
+
+    seq_w_variants += ref_seq[curr_index:]
 
     ref_correct_ratio = ref_correct / len(row["REF"])
     return dict(
@@ -100,7 +149,7 @@ def apply_variants_to_a_loci(
 def postprocess_agg_variants(agg_variants_df: pd.DataFrame) -> pd.DataFrame:
     # remove redundant columns
     agg_variants_df = agg_variants_df.drop(
-        columns=["REF", "ALT", "GENOME_INDEX", "IS_INDEL"]
+        columns=["REF", "ALT", "GENOME_INDEX", "IS_INDEL", "loci_nucleotide_nr"]
     )
     # clean up the columns
     agg_variants_df["seq_w_variants"] = agg_variants_df[
@@ -155,8 +204,6 @@ def run(
         lambda row: apply_variants_to_a_loci(
             ref_seq=genomic_loci_dict[row["loci"]]["ref_seq"],
             strand=genomic_loci_dict[row["loci"]]["strand"],
-            start=genomic_loci_dict[row["loci"]]["start"],
-            end=genomic_loci_dict[row["loci"]]["end"],
             row=row,
         ),
         axis=1,
@@ -176,4 +223,6 @@ def run(
 
     # save agg variants
     logging.info("Saving agg variants")
-    agg_variants_df.to_parquet(os.path.join(output_dir, "agg_variants.parquet"))
+    agg_variants_df.to_parquet(
+        os.path.join(output_dir, "agg_variants_md_cnn.parquet")
+    )
