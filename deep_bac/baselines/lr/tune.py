b63692c2b1bc791d869e5de1af87a4bdffb4214c
macwiatrak <macwiatrak@gmail.com>
2023-03-27 18:05:01 +0100

    feat: add main function for tuning

--- deep_bac/baselines/lr/tune.py
+++ deep_bac/baselines/lr/tune.py
@@ -1,7 +1,10 @@
+import json
+import logging
+import os
 from typing import Dict, List, Literal
 
 import pandas as pd
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, load_npz
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 
@@ -9,6 +12,10 @@ from deep_bac.baselines.lr.data_reader import (
     split_train_val_test,
     get_drug_var_matrices,
 )
+
+logging.basicConfig(level=logging.INFO)
+
+INPUT_DIR = "/Users/maciejwiatrak/Desktop/bacterial_genomics/cryptic/data/"
 
 
 def tune(
@@ -43,13 +50,20 @@ def tune(
 def run(
     drug_idx: int,
     train_test_split_unq_ids_file_path: str,
-    variant_matrix: csr_matrix,
-    unq_id_to_idx: Dict[str, int],
+    variant_matrix_input_dir: str,
     df_unq_ids_labels: pd.DataFrame,
     params: Dict[str, List],
     max_iter: int,
     penalty: Literal["l1", "l2", "elasticnet"] = None,
 ):
+    variant_matrix = load_npz(
+        os.path.join(variant_matrix_input_dir, "var_matrix.npz")
+    )
+    with open(
+        os.path.join(variant_matrix_input_dir, "unq_id_to_idx.json"), "r"
+    ) as f:
+        unq_id_to_idx = json.load(f)
+
     model = LogisticRegression(
         max_iter=max_iter,
         penalty=penalty,
@@ -64,3 +78,26 @@ def run(
         parameters=params,
     )
     return best_params
+
+
+def main():
+
+    best_params = run(
+        drug_idx=0,
+        train_test_split_unq_ids_file_path=os.path.join(
+            INPUT_DIR, "train_test_cv_split_unq_ids.json"
+        ),
+        variant_matrix_input_dir="",
+        df_unq_ids_labels=pd.read_parquet(
+            os.path.join(
+                INPUT_DIR, "phenotype_labels_with_binary_labels.parquet"
+            )
+        ),
+        params={
+            "C": [0.0001, 0.001, 0.01, 0.1, 0.2, 0.5],
+            "class_weight": [None, "balanced"],
+        },
+        penalty=None,
+        max_iter=1000,
+    )
+    print(best_params)
