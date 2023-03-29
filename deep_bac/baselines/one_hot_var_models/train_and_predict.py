b53bdb20f7f461eefd86b7b622a76aec39a29488
macwiatrak <macwiatrak@gmail.com>
2023-03-29 14:57:13 +0100

    feat: update l1_ratio and alpha

--- deep_bac/baselines/one_hot_var_models/train_and_predict.py
+++ deep_bac/baselines/one_hot_var_models/train_and_predict.py
@@ -24,8 +24,8 @@ def get_tuning_params(penalty: Literal["l1", "l2", "elasticnet"] = "l2"):
     if penalty == "l1":
         return {"C": [0.0001, 0.001, 0.01, 0.1, 1.0]}
     return {
-        "l1_ratio": [0.25, 0.5, 0.75],
-        "alpha": [0.1, 0.25, 0.5, 1.0, 2.0],
+        "l1_ratio": [0.001, 0.01, 0.1, 0.25, 0.5],
+        "alpha": [0.001, 0.01, 0.1, 0.25, 0.5],
     }
 
 
