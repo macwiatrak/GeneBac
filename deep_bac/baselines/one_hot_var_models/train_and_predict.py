fde770cf8f411991c3ea67fba415c7781804f6b9
macwiatrak <macwiatrak@gmail.com>
2023-03-29 14:46:11 +0100

    feat: remove l1_ratio=0.0 from elasticnet

--- deep_bac/baselines/one_hot_var_models/train_and_predict.py
+++ deep_bac/baselines/one_hot_var_models/train_and_predict.py
@@ -24,7 +24,7 @@ def get_tuning_params(penalty: Literal["l1", "l2", "elasticnet"] = "l2"):
     if penalty == "l1":
         return {"C": [0.0001, 0.001, 0.01, 0.1, 1.0]}
     return {
-        "l1_ratio": [0.0, 0.25, 0.5, 0.75, 1.0],
+        "l1_ratio": [0.25, 0.5, 0.75],
         "alpha": [0.1, 0.25, 0.5, 1.0, 2.0],
     }
 
