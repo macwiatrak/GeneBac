c26877235c632a4f39684a7b5ebbc4bb8605752d
macwiatrak <macwiatrak@gmail.com>
2023-04-03 17:19:20 +0100

    feat: continue working on linear pytorch model

--- deep_bac/train_gene_pheno.py
+++ deep_bac/train_gene_pheno.py
@@ -99,22 +99,22 @@ def run(
 
     if test_after_train:
         logging.info("Testing after training without computing thresholds")
-        _ = trainer.test(
+        return trainer.test(
             model,
             dataloaders=data.test_dataloader,
             ckpt_path="best",
         )
 
-        logging.info("Testing after training after computing thresholds")
-        model = model.load_from_checkpoint(
-            trainer.checkpoint_callback.best_model_path
-        )
-        drug_thresholds = get_drug_thresholds(model, data.train_dataloader)
-        model.drug_thresholds = drug_thresholds
-        return trainer.test(
-            model,
-            dataloaders=data.test_dataloader,
-        )
+        # logging.info("Testing after training after computing thresholds")
+        # model = model.load_from_checkpoint(
+        #     trainer.checkpoint_callback.best_model_path
+        # )
+        # drug_thresholds = get_drug_thresholds(model, data.train_dataloader)
+        # model.drug_thresholds = drug_thresholds
+        # return trainer.test(
+        #     model,
+        #     dataloaders=data.test_dataloader,
+        # )
     return None
 
 
